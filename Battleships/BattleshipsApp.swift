18ad5448a4719da2bbfe0fc0f7f0fdd9979f4f19
Mat <mateuszzeu@gmail.com>
2025-11-27 17:48:36 +0100

    Initial Commit

---
+++ Battleships/BattleshipsApp.swift
@@ -0,0 +1,17 @@
+//
+//  BattleshipsApp.swift
+//  Battleships
+//
+//  Created by MAT on 27/11/2025.
+//
+
+import SwiftUI
+
+@main
+struct BattleshipsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
